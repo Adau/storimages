d43edf9c318dbcff4300af22f36b01bfc95b1083
Adrien <adrien.dau@gmail.com>
2018-03-22 10:27:03 +0100

    Modifier la configuration d'ESLint
    Utiliser ES6 et les modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   "extends": "eslint:recommended",
   "env": {
     "browser": true,
+    "jquery": true,
     "node": true,
     "es6": true
   },
   "parserOptions": {
+    "ecmaVersion": 6,
     "sourceType": "module"
   },
   "rules": {
-    "semi": ["error", "always"]
   }
 };
